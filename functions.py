8c9fd8d29d544804d45a6abbda316ab6a151d29b
Satyam Thube <satyamthube39@gmail.com>
2026-05-17 22:08:09 +0530

    Added functions, lists, and marks project

---
+++ functions.py
@@ -0,0 +1,26 @@
+def greet():
+    print("Welcome")
+    print("Keep learning")
+
+greet()
+
+
+
+def greet(name):
+    print("Hello", name)
+
+greet("Satyam")
+
+
+
+def add_numbers(a, b):
+    print("Addition =", a + b)
+
+add_numbers(5, 3)
+
+
+
+def athlete_message(name):
+    print(name, "keep training hard!")
+
+athlete_message("Satyam")
