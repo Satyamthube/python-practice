b8787a069ecc120467d567ad56c27800097412f5
Satyam Thube <satyamthube39@gmail.com>
2026-05-17 22:08:41 +0530

    Added functions, lists, and marks project

---
+++ lists.py
@@ -0,0 +1,21 @@
+players = ["Virat", "Rohit", "Gill"]
+print(players)
+
+
+players = ["Virat", "Rohit", "Gill"]
+print(players[0])
+
+
+players = ["Virat", "Rohit", "Gill"]
+for player in players:
+    print(player)
+
+
+players = ["Virat", "Rohit"]
+players.append("Gill")
+print(players)
+
+
+foods = ["Rice", "Dal", "Chapati"]
+for food in foods:
+    print(food)
