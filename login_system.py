9d5540d0c15c36fcb75a99981fc3166edc7ccab0
Satyam Thube <satyamthube39@gmail.com>
2026-05-20 22:17:23 +0530

    Delete login_system.py

--- login_system.py
+++
@@ -1,7 +0,0 @@
-username=(input('enter username '))
-password=(input('enter password '))
-
-if username=='satyam13' and password=='sattu22':
-    print('login successful')
-else:
-    print('incorrect username or password')
