afb88907c41da863fe12c50648862ceec695e352
Satyam Thube <satyamthube39@gmail.com>
2026-05-28 21:36:46 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,19 @@
+#Math module
+import math
+print(math.sqrt(25))
+
+#Random Module
+import random
+print(random.randint(1, 10))
+
+#practice question 3
+#Generate random number between 1–100.\
+import random
+print(random.randint(1, 100))
+
+#practice question 4
+#Use math module to: find square root. find power. round decimal number.
+import math
+print(math.sqrt(36))
+print(math.pow(100,2))
+print(round(5.3256,2))
